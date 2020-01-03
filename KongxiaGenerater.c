3f180424b12bc22b1f9f7fbea949e320fd79cfc7
Yanshiqwq <56720055+Yanshiqwq@users.noreply.github.com>
2020-01-03 14:55:19 +0800

    Add files via upload

---
+++ KongxiaGenerater.c
@@ -0,0 +1,219 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+int main()
+{
+    int count;
+    srand((unsigned)time(NULL));
+    printf("输入空夏次数:");
+    scanf("%d",&count);
+    system("cls");
+    printf("Creeper?\nAww~man~\n");
+    for(int x=0;x<count-2;x++)
+    {
+        int KongxiaNum=rand()%100+1;
+        if(KongxiaNum==1)
+            printf("Creeper?\n");
+        if(KongxiaNum==2)
+            printf("awwwww man\n");
+        if(KongxiaNum==3)
+            printf("so we back in the mine\n");
+        if(KongxiaNum==4)
+            printf("打破\n你们重来叭\n");
+        if(KongxiaNum==5)
+            printf("aw man!\n");
+        if(KongxiaNum==6)
+            printf("se no\n");
+        if(KongxiaNum==7)
+            printf("got our pickaxe swinging from\n");
+        if(KongxiaNum==8)
+            printf("side to side\n");
+        if(KongxiaNum==9)
+            printf("balabala\n");
+        if(KongxiaNum==10)
+            printf("单走一个六\n"); 
+        if(KongxiaNum==11)
+            printf("你今天十七张牌能秒我?\n");
+        if(KongxiaNum==12)
+            printf("打乱阵型\n");
+        if(KongxiaNum==13)
+            printf("得得得得得得得得得得得得得得\n");
+        if(KongxiaNum==14)
+            printf("This task is a grueling one\n");
+        if(KongxiaNum==15)
+            printf("hope to find some diamonds to\n");
+        if(KongxiaNum==16)
+            printf("night night night\n");
+        if(KongxiaNum==17)
+            printf("我和我的祖~国~一刻也不能分割~\n让他唱\n让他唱!\n");
+        if(KongxiaNum==18)
+            printf("?\n");
+        if(KongxiaNum==19)
+            printf("艹\n草 本 植 物\n");
+        if(KongxiaNum==20)
+            printf("无论我走到哪~里 都流出一首~ ~赞歌\n让他唱\n别停!\n让!    他!    唱!\n让他唱!\n");
+        if(KongxiaNum==21)
+            printf("改革春风吹满地~\n");
+        if(KongxiaNum==22)
+            printf("中果人民真争气~\n");
+        if(KongxiaNum==23)
+            printf("中果人民真争气~\nChinese people so niubi\n");
+        if(KongxiaNum==24)
+            printf("这个世界太疯狂~\n");
+        if(KongxiaNum==25)
+            printf("耗子都给猫当伴娘\n");
+        if(KongxiaNum==26)
+            printf("齐德隆~\n齐东强~\n齐东隆咚隆咚强~\n");
+        if(KongxiaNum==27)
+            printf("苏卡不列\n");
+        if(KongxiaNum==28)
+            printf("请停止你的空夏行为\n");
+        if(KongxiaNum==29)
+            printf("吔屎啦你\n");
+        if(KongxiaNum==30)
+            printf("木大木大木大木大木大木大木大木大木大木大木大木大木大木大木大木大木大木大\n");
+        if(KongxiaNum==31)
+            printf("欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉欧拉\n");
+        if(KongxiaNum==32)
+            printf("舒克舒克开飞机\n");
+        if(KongxiaNum==33)
+            printf("奥力给!\n");
+        if(KongxiaNum==34)
+            printf("给阿姨倒一杯卡布奇诺\n");
+        if(KongxiaNum==35)
+            printf("猪油骨~拿来卤~你又忘洗蛋~\n");
+        if(KongxiaNum==36)
+            printf("炖了那湖鲫~\n");
+        if(KongxiaNum==37)
+            printf("不过我~煮了袋~速食面~\n");
+        if(KongxiaNum==38)
+            printf("肥料就要金坷垃~一袋能顶两袋撒~\n");
+        if(KongxiaNum==39)
+            printf("我不要你觉得，我要我觉得\n");
+        if(KongxiaNum==40)
+            printf("反手来个超级加倍\n");
+        if(KongxiaNum==41)
+            printf("带你老妈飞～\n");
+        if(KongxiaNum==42)
+            printf("啊～我搞大伟\n");
+        if(KongxiaNum==43)
+            printf("妈妈我想吃烤山药\n");
+        if(KongxiaNum==44)
+            printf("se～no(看不到我)\n↑看到你了\n");
+        if(KongxiaNum==45)
+            printf("♂ass♂ass♂ass♂ass♂ass♂\n");
+        if(KongxiaNum==46)
+            printf("Are you ok？\n");
+        if(KongxiaNum==47)
+            printf("卢本伟牛逼！！！\n");
+        if(KongxiaNum==48)
+            printf("要素过多\n");
+        if(KongxiaNum==49)
+            printf("Se no dei mo sou jia na mei\n");
+        if(KongxiaNum==50)
+            printf("窝窝头\n");
+        if(KongxiaNum==51)
+            printf("来，左边儿跟我一起画个龙\n");
+        if(KongxiaNum==52)
+            printf("ko↘ko→da↘yo↗\n");
+        if(KongxiaNum==53)
+            printf("逮虾户\n");
+        if(KongxiaNum==54)
+            printf("窝窝头，一块钱四个～\n");
+        if(KongxiaNum==55)
+            printf("ko～ko～da～yo～\n");
+        if(KongxiaNum==56)
+            printf("de mo so na ja da ne\n");
+        if(KongxiaNum==57)
+            printf("改♂革春风吹满地\n");
+        if(KongxiaNum==58)
+            printf("真香\n");
+        if(KongxiaNum==59)
+            printf("百因必有果，你的报应就是我\n");
+        if(KongxiaNum==60)
+            printf("Please stop your kongxia behavior\n");
+        if(KongxiaNum==61)
+            printf("阿里阿里阿里阿里阿里阿里阿里阿里阿里阿里阿里阿里\n");
+        if(KongxiaNum==62)
+            printf("Ss2s\n");
+        if(KongxiaNum==63)
+            printf("夜太美~\n");
+        if(KongxiaNum==64)
+            printf("fucker~\n");
+        if(KongxiaNum==65)
+            printf("你看这个面它又长又宽\n");
+        if(KongxiaNum==66)
+            printf("第一次鸦片战争养的我\n");
+        if(KongxiaNum==67)
+            printf("闹~~太~~套~~\n");
+        if(KongxiaNum==68)
+            printf("如果我开挖掘机你会爱我吗\n");
+        if(KongxiaNum==69)
+            printf("雞你太美\n");
+        if(KongxiaNum==70)
+            printf("辣妹儿～\n");
+        if(KongxiaNum==71)
+            printf("wryyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy\n");
+        if(KongxiaNum==72)
+            printf("毛闷台\n");
+        if(KongxiaNum==73)
+            printf("反复横跳\n");
+        if(KongxiaNum==74)
+            printf("为祖国奏响伟大复兴的华章\n");
+        if(KongxiaNum==75)
+            printf("一刀流奥义\n");
+        if(KongxiaNum==76)
+            printf("我们遇到什么困难也不要怕微笑着面对他消除恐惧的最好方法就是面对恐惧坚持才是胜利 加油!奥利给!\n");
+        if(KongxiaNum==77)
+            printf("鸡你太美\n");
+        if(KongxiaNum==78)
+            printf("so no chi no sa da mai jo——————————jo——————————\n");
+        if(KongxiaNum==79)
+            printf("坤坤变篮球～\n");
+        if(KongxiaNum==80)
+            printf("mo u so n na n ja ho ra\n");
+        if(KongxiaNum==81)
+            printf("ko ko ro wa shi n ka su ru yo mo to mo to\n");
+        if(KongxiaNum==82)
+            printf("烤面筋～\n");
+        if(KongxiaNum==83)
+            printf("砸瓦鲁多～\n");
+        if(KongxiaNum==84)
+            printf("洗海带哟～洗海带哟～\n");
+        if(KongxiaNum==85)
+            printf("金坷垃，你们日本别想啦！\n");
+        if(KongxiaNum==86)
+            printf("???d????\n");
+        if(KongxiaNum==87)
+            printf("鱼~好大的鱼~虎纹鲨鱼~\n");
+        if(KongxiaNum==88)
+            printf("搞比利♂\n");
+        if(KongxiaNum==89)
+            printf("仙人指路\n");
+        if(KongxiaNum==90)
+            printf("阿姨压一压\n");
+        if(KongxiaNum==91)
+            printf("他没有头去加拿大\n");
+        if(KongxiaNum==92)
+            printf("敌我之间分不清~崩坏的礼乐里~失了明~\n");
+        if(KongxiaNum==93)
+            printf("路过的假面骑士\n");
+        if(KongxiaNum==94)
+            printf("本无关是福祸\n");
+        if(KongxiaNum==95)
+            printf("开始你的炸弹表演秀\n");
+        if(KongxiaNum==96)
+            printf("谁TM买小米？\n");
+        if(KongxiaNum==97)
+            printf("这就是你分手的借口~\n");
+        if(KongxiaNum==98)
+            printf("嗯哼哼啊啊啊啊啊啊啊啊啊啊啊\n");
+        if(KongxiaNum==99)
+            printf("忽然恶臭\n");
+        if(KongxiaNum==100)
+            printf("啊噗噜派\n");
+    }
+    system("pause");
+    return 0;
+}
